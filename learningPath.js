b0e76df8a25514fa5b9b65de986c355b796b45d5
Naomi Mine <naomi.mine@gmail.com>
2017-06-08 17:20:29 -0500

    have array of levels

--- learningPath.js
+++ learningPath.js
@@ -1,23 +1,34 @@
-console.log("lalala")
 
 var Papa = require('babyparse');
 var fs = require('fs');
 var file = 'data/domain_order.csv';
 
-var kindergarten = [];
+var sampleStudent = [5,5,5,2]
+min = Math.min.apply(Math, sampleStudent)
+console.log(min, "this is min");
+
+var levels = [];
 
 var content = fs.readFileSync(file, { encoding: 'binary' });
 Papa.parse(content, {
-	preview: 1,
     step: function(row){
-    	var firstRow = row.data[0];
-        kindergarten.push(firstRow)
-        console.log(kindergarten, "kindergarten")
+    	var rowData = row.data[0];
+    	var gradeLevel = rowData[0];
+    	rowData.shift();
+    	for(i=0; i<rowData.length; i++){
+    		var j = rowData[i].split("");
+    		j.unshift(gradeLevel);
+    		var k = j.join("");
+    		levels.push(k);
+    	}
     }
 });
 
+console.log(levels, "these are the levels");
 
-
+//with sample student:
+	//sort through 1-4 in array and find lowest number;
+	//get up to grade level in that category: (2L, 3L, 4L) 5RI, 5RL
 
 //objective: take in data from both files and produce learning path based on students' current levels in 4 categories
 //first:

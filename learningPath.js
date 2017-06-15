8cafabfc07209c9571146b99461ec9fca62b6346
Naomi Mine <naomi.mine@gmail.com>
2017-06-14 21:15:46 -0500

    changing variable names

--- learningPath.js
+++ learningPath.js
@@ -5,9 +5,9 @@ var file2 = 'data/student_tests.csv';
 var domainOrderFile = fs.readFileSync(file1, { encoding: 'binary' });
 var studentTestFile = fs.readFileSync(file2, { encoding: 'binary' });
 
-var categories = [];
-var grade = [];
-var topic = [];
+var domainHeaders = [];
+var learningPlan = [];
+var domainList = [];
 var students = [];
 var testArray = [];
 
@@ -18,32 +18,33 @@ Papa.parse(domainOrderFile, {
     	var gradeLevel = rowData[0];
     	rowData.shift();
     	for(i=0; i<rowData.length; i++){
-    		grade.push([gradeLevel])
-    		topic.push(rowData[i]);
+    		learningPlan.push([gradeLevel])
+    		domainList.push(rowData[i]);
     	}
     }
 });
+
 
 
 Papa.parse(studentTestFile, {
 	header: true,
 	step: function(row){
 		var rowData = row.data[0];
-		var studentInfo = [];
+		var studentTestInfo = [];
 		testInfo = [];
-		category = [];
+		headers = [];
 		for(key in rowData){
-			studentInfo.push(rowData[key]);
-			category.push(key);
+			studentTestInfo.push(rowData[key]);
+			headers.push(key);
 		}
-		for(var i=1;i<studentInfo.length;i++){
-			testInfo.push([studentInfo[i]]);
+		for(var i=1;i<studentTestInfo.length;i++){
+			testInfo.push([studentTestInfo[i]]);
 		}
 		testArray.push(testInfo);
-		students.push(studentInfo[0])
-		for(i = 1; i < category.length; i++){
-			if(categories.length < category.length - 1) {
-				categories.push(category[i]);
+		students.push(studentTestInfo[0])
+		for(i = 1; i < headers.length; i++){
+			if(domainHeaders.length < headers.length - 1) {
+				domainHeaders.push(headers[i]);
 			}
 		}
 	}
@@ -51,7 +52,7 @@ Papa.parse(studentTestFile, {
 
 
 function producePlan(student){
-	var slicedGrade = grade.slice();
+	var learninglearningPlanCopy = learningPlan.slice();
 	var expStudent =[];
 	var currGrade = 0;
 	var currTopic = "";
@@ -59,31 +60,27 @@ function producePlan(student){
 	for(var i=0;i<student.length;i++){
 		expStudent.push([
 			student[i][0],
-			categories[i]
+			domainHeaders[i]
 		]);
 	}
 	for(var i=0;i<student.length;i++){
 		currGrade = parseInt(expStudent[i][0]);
 		currTopic = expStudent[i][1];
-		for(var j=0;j<slicedGrade.length;j++){
-			if(slicedGrade[j][0] === 'K'){
-				slicedGrade[j][0] = 0;
+		for(var j=0;j<learningPlanCopy.length;j++){
+			if(learningPlanCopy[j][0] === 'K'){
+				learningPlanCopy[j][0] = 0;
 			}
-			if((slicedGrade[j][0] < currGrade) && (currTopic === slicedGrade[j][1])){
-				slicedGrade[j] = "nada";
+			if((learningPlanCopy[j][0] < currGrade) && (currTopic === learningPlanCopy[j][1])){
+				learningPlanCopy[j] = "nada";
 			} 
 		}
 	}
-	for(i=0;i<slicedGrade.length;i++){
-		if(plan.length < 5) {
-			if(slicedGrade[i] !== "nada"){
-				if(slicedGrade[i] !== undefined){
-					if(slicedGrade[i][0] === 0){
-						slicedGrade[i][0] = 'K';
-					}
-					plan.push(slicedGrade[i].join("."));
-				}
+	for(i=0;i<learningPlanCopy.length;i++){
+		if((plan.length < 5) && (learningPlanCopy[i] !== "nada") && (learningPlanCopy[i] !== undefined)) {
+			if(learningPlanCopy[i][0] === 0){
+				learningPlanCopy[i][0] = 'K';
 			}
+			plan.push(learningPlanCopy[i].join("."));
 		}
 	}
 	process.stdout.write(plan.join(", ") + "\n");
@@ -92,8 +89,8 @@ function producePlan(student){
 
 
 function initiateApp(){
-	for(var i=0;i<grade.length;i++){
-		grade[i].splice(1, 0, topic[i])
+	for(var i=0;i<learningPlan.length;i++){
+		learningPlan[i].splice(1, 0, domainList[i])
 	}
 	for(var i = 0; i < testArray.length; i++){
 		process.stdout.write(students[i] + ": ");
@@ -108,10 +105,10 @@ initiateApp();
 module.exports = {
 	initiateApp: initiateApp,
 	producePlan: producePlan,
-	grade: grade,
+	learningPlan: learningPlan,
 	testArray: testArray,
 	students: students,
-	categories: categories,
-	topic: topic
+	domainHeaders: domainHeaders,
+	domainList: domainList
 }
 
